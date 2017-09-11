3078529e8b6630742ba9515e53f441214f5dcea9
Kirk Henderson <kirk733@me.com>
2017-09-10 21:21:45 -0600

    commit working files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
